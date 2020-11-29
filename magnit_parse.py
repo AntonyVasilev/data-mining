ae06ace79528fe249b2540c570607c6aa1eaa4fc
Antony Vasilev <antony.n.vasilev@gmail.com>
2020-11-29 22:24:22 +0300

    added date_from & date_to into product_template dict

--- magnit_parse.py
+++ magnit_parse.py
@@ -7,10 +7,11 @@ import pymongo
 import dotenv
 from urllib.parse import urljoin
 
-#dotenv.load_dotenv('.env')
+
+# dotenv.load_dotenv('.env')
 
 # client = pymongo.MongoClient(os.getenv('DATA_BASE'))
-        # self.db = client['parse_les2']
+# self.db = client['parse_les2']
 
 
 class MagnitParse:
@@ -21,16 +22,52 @@ class MagnitParse:
     def __init__(self, start_url):
         self.start_url = start_url
 
+        self.months = {
+            'января': '01',
+            'февраля': '02',
+            'марта': '03',
+            'апреля': '04',
+            'мая': '05',
+            'июня': '06',
+            'июля': '07',
+            'августа': '08',
+            'сентября': '09',
+            'октября': '10',
+            'ноября': '11',
+            'декабря': '12'
+        }
+
         self.product_template = {
             'url': lambda soup: urljoin(self.start_url, soup.get('href')),
             'promo_name': lambda soup: soup.find('div', attrs={'class': 'card-sale__header'}).text,
             'product_name': lambda soup: soup.find('div', attrs={'class': 'card-sale__title'}).text,
-            'old_price': lambda soup: float(f"{soup.find('div', attrs={'class': 'label__price_old'}).contents[1].text}.{soup.find('div', attrs={'class': 'label__price_old'}).contents[3].text}"),
-            'new_price': lambda soup: float(f"{soup.find('div', attrs={'class': 'label__price_new'}).contents[1].text}.{soup.find('div', attrs={'class': 'label__price_new'}).contents[3].text}"),
+            'old_price': lambda soup: self._get_price(soup, 'old'),
+            'new_price': lambda soup: self._get_price(soup, 'new'),
             'image_url': lambda soup: urljoin(self.start_url, soup.find('img').get('data-src')),
-            'date_from': '',
-            'date_to': ''
+            'date_from': lambda soup: self._get_dates(soup, 'from'),
+            'date_to': lambda soup: self._get_dates(soup, 'to')
         }
+
+    @staticmethod
+    def _get_price(soup, price_type):
+        if price_type == 'old':
+            response = soup.find('div', attrs={'class': 'label__price_old'})
+        elif price_type == 'new':
+            response = soup.find('div', attrs={'class': 'label__price_new'})
+        else:
+            return
+        return float(f'{response.contents[1].text}.{response.contents[3].text}')
+
+    def _get_dates(self, soup, date_type):
+        response = soup.find('div', attrs={'class': 'card-sale__date'})
+        if date_type == 'from':
+            date = response.contents[1].text[2:4]
+            month = self.months[response.contents[1].text[5:]]
+        elif date_type == 'to':
+            date = response.contents[3].text[3:5]
+            month = self.months[response.contents[3].text[6:]]
+        year = dt.datetime.now().year
+        return dt.datetime.strptime(f'{date}/{month}/{year}', '%d/%m/%Y')
 
     @staticmethod
     def _get(*args, **kwargs):
@@ -51,7 +88,7 @@ class MagnitParse:
         soup = self.soup(self.start_url)
         for product in self.parse(soup):
             print(1)
-            #self.save(product)
+            # self.save(product)
 
     def parse(self, soup):
         catalog = soup.find('div', attrs={'class': 'сatalogue__main'})
