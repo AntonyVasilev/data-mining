19654e5d6681fbdb56314c02c52c07c0e696abff
Antony Vasilev <antony.n.vasilev@gmail.com>
2020-12-19 22:17:21 +0300

    added handshakes finding into MutualFriends & added MutualFriends calling into main.py

--- main.py
+++ main.py
@@ -2,15 +2,29 @@ import os
 from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
 from gb_parse.spiders.instagram import InstagramSpider
+from mutual_friends import MutualFriends
 
 import dotenv
 
 dotenv.load_dotenv('.env')
 
 if __name__ == '__main__':
+    users_list = ['mr.proghammer', 'codeforgeyt']
     crawl_settings = Settings()
     crawl_settings.setmodule('gb_parse.settings')
     crawl_proc = CrawlerProcess(settings=crawl_settings)
-    crawl_proc.crawl(InstagramSpider, login=os.getenv('LOGIN'), password=os.getenv('PASSWORD'),
-                     users_list=['codeforgeyt', 'mr.proghammer'])
-    crawl_proc.start()
+    while True:
+        crawl_proc.crawl(InstagramSpider, login=os.getenv('LOGIN'), password=os.getenv('PASSWORD'),
+                         users_list=users_list)
+        crawl_proc.start()
+        mutual_proc = MutualFriends()
+        handshakes, mutual_friends_list = mutual_proc.run(users_list)
+        if handshakes:
+            break
+        else:
+            users_list.clear()
+            for mutual_friends in mutual_friends_list:
+                for value in mutual_friends.values():
+                    users_list.append(value)
+
+    print(users_list, handshakes)
--- mutual_friends.py
+++ mutual_friends.py
@@ -7,32 +7,47 @@ class MutualFriends:
     def __init__(self):
         self.db = MongoClient()['parse_gb']
 
-    def run(self, user_id):
-        follow_ids_list = self.get_follow_list(user_id)
-        mutual_friends = self.get_mutual_friends(user_id, follow_ids_list)
-        print(mutual_friends)
+    def run(self, user_names):
+        mutual_friends_list = []
+        for user_name in user_names:
+            follow_ids_list = self.get_follow_list(user_name)
+            mutual_friends = self.get_mutual_friends(user_name, follow_ids_list)
+            mutual_friends_list.append(mutual_friends)
 
-    def get_follow_list(self, user_id) -> List:
+        handshakes = self.get_handshakes(mutual_friends_list)
+        print(handshakes)
+        # return handshakes, mutual_friends_list
+
+    def get_follow_list(self, user_name) -> List:
         collection = self.db['instagram']
         follow_ids_list = []
-        for item in collection.find({'user_id': user_id, 'type': 'follow'}):
+        for item in collection.find({'user_name': user_name, 'type': 'follow'}):
             follow_ids_list.append(item['follow_id'])
-        print(len(follow_ids_list))
+        # print(len(follow_ids_list))
         return follow_ids_list
 
-    def get_mutual_friends(self, user_id, follow_ids_list) -> Dict:
+    def get_mutual_friends(self, user_name, follow_ids_list) -> Dict:
         mutual_friends_dict = {}
         collection = self.db['instagram']
-        for item in collection.find({'user_id': user_id, 'type': 'followed'}):
+        for item in collection.find({'user_name': user_name, 'type': 'followed'}):
             if item['followed_id'] in follow_ids_list:
                 mutual_friends_dict[item['followed_id']] = item['followed_name']
-        print(len(mutual_friends_dict))
+        # print(len(mutual_friends_dict))
         return mutual_friends_dict
+
+    @staticmethod
+    def get_handshakes(mutual_friends_list):
+        results_list = []
+        list_len = len(mutual_friends_list)
+        for i in range(list_len - 1):
+            temp_list = []
+            for user_id, user_name in mutual_friends_list[i].items():
+                if user_id in mutual_friends_list[i + 1].keys():
+                    temp_list.append({user_id: user_name})
+            results_list.append(temp_list)
+        return results_list
 
 
 if __name__ == '__main__':
     proghamme = MutualFriends()
-    proghamme.run('36636112793')
-
-    codeforgeyt = MutualFriends()
-    codeforgeyt.run('8650028000')
+    proghamme.run(['mr.proghammer', 'codeforgeyt'])
