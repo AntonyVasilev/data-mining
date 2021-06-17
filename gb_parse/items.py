9f470a4e40c1e47e86870acaeaba90c7acaa9149
Antony Vasilev <antony.n.vasilev@gmail.com>
2021-06-17 09:52:26 +0300

    removed old files

--- gb_parse/items.py
+++ gb_parse/items.py
@@ -6,36 +6,6 @@
 import scrapy
 
 
-class GbParseItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
-
-
-class AutoYoulaItem(scrapy.Item):
-    _id = scrapy.Field()
-    title = scrapy.Field()
-    images = scrapy.Field()
-    description = scrapy.Field()
-    url = scrapy.Field()
-    author = scrapy.Field()
-    specifications = scrapy.Field()
-
-
-class HhRemoteItem(scrapy.Item):
-    _id = scrapy.Field()
-    url = scrapy.Field()
-    title = scrapy.Field()
-    name = scrapy.Field()
-    salary = scrapy.Field()
-    description = scrapy.Field()
-    skills = scrapy.Field()
-    employer_url = scrapy.Field()
-    ext_url = scrapy.Field()
-    areas_of_activity = scrapy.Field()
-    employer_description = scrapy.Field()
-
-
 class InstagramMutual(scrapy.Item):
     _id = scrapy.Field()
     user_name = scrapy.Field()
